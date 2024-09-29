a1812fc4fbe6d1ac4fdc88148018843972e94868
Alexander Balaban <balaban.alexander+github@gmail.com>
2024-09-29 20:08:10 +0100

    bencode decoder

--- app/main.py
+++ app/main.py
@@ -1,44 +1,85 @@
 import json
 import sys
 
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
 
-# Examples:
-#
-# - decode_bencode(b"5:hello") -> b"hello"
-# - decode_bencode(b"10:hello12345") -> b"hello12345"
-def decode_bencode(bencoded_value):
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+def decode_bencode(value: bytes, offset: int): 
+    def _parse_number() -> int:
+        nonlocal offset
+        val = 0 
+        while chr(value[offset]).isdigit():
+            val *= 10
+            val += int(chr(value[offset]))
+            offset += 1
+        return val 
+        
+    def decode_integer() -> int:
+        nonlocal offset
+        # skip 'i'
+        offset += 1
+        val = _parse_number()
+        # skip 'e'
+        offset += 1
+        return val
+        
+    def decode_string() -> str:
+        nonlocal offset
+        l = _parse_number()
+        # skip ':'
+        offset += 1
+        s = str(value[offset: offset + l], encoding='utf-8')
+        # skip str
+        offset += l
+        return s
+        
+    def decode_list() -> list[any]:
+        nonlocal offset
+        # skip 'l'
+        offset += 1 
+        l = [] 
+        while value[offset] != ord('e'):
+            val, offset = decode_bencode(value, offset)
+            l.append(val) 
+        # skip 'e' 
+        offset += 1
+        return l
+        
+    def decode_dict() -> dict[any, any]:
+        nonlocal offset
+        # skip 'd' 
+        offset += 1 
+        d = {} 
+        while value[offset] != ord('e'):
+            k, offset = decode_bencode(value, offset)
+            v, offset = decode_bencode(value, offset)
+            d[k] = v 
+        # skip 'e'
+        offset += 1 
+        return d
+
+
+    if value[offset] == ord('i'):
+        return (decode_integer(), offset)
+    elif value[offset] == ord('l'):
+        return (decode_list(), offset)
+    elif value[offset] == ord('d'):
+        return (decode_dict(), offset)
+    else: 
+        return (decode_string(), offset)
 
 
 def main():
     command = sys.argv[1]
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
 
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
         def bytes_to_str(data):
             if isinstance(data, bytes):
                 return data.decode()
-
             raise TypeError(f"Type not serializable: {type(data)}")
 
-        # Uncomment this block to pass the first stage
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        val, _ = decode_bencode(bencoded_value, 0)
+        print(json.dumps(val, default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
